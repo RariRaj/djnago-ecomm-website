c66dc97b99fb9cd1144a393910557566669586ab
Rari Raj <rariraj95@gmail.com>
2020-09-28 23:20:08 +0530

    Ecomm project-Django

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+from .models import *
+
+# Create your views here.
+def store(request):
+    products=Product.objects.all()
+    context={'products':products}
+    return render(request,'store.html',context)
+
+def cart(request):
+    return render(request,'cart.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
